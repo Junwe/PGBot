3cadb7e80ac95b25d2c9a78102e22b743c72974c
Junwe <junwe99@gmail.com>
2020-02-19 14:16:47 +0900

    외부에서 데이터 읽어오게 변경

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ client = discord.Client()
 messageText = 'none message'
 datalist = []
 
+
 @client.event
 async def on_ready():
     global messageText
@@ -15,7 +16,7 @@ async def on_ready():
     print('logged in as')
     print(client.user.name)
     print(client.user.id)
-    channel = client.get_channel(datalist[1])
+    channel = client.get_channel(int(datalist[1]))
     await channel.send(embed=discord.Embed(description=messageText))
     await client.logout()
     print('----------------')
@@ -23,14 +24,19 @@ async def on_ready():
 
 def ReadBotData():
     global datalist
-    f = open("/Users/shj/Desktop/UnityProject/Project_JPER/JPER_Bot/PGBot/data.txt",
+    f = open("/Users/shj/Desktop/UnityProject/Project_JPER/JPER_Bot/PGBot/data.bin",
              mode='r', encoding='UTF-8')
 
     lines = f.readlines()
 
     for line in lines:
-        data =  line.split(':')
-        datalist.append(data[1][:-1])
+        data = line.split(':')
+        if "\n" in data[1]:
+            datalist.append(data[1][:-1])
+        else:
+            datalist.append(data[1])
+
+    print(datalist)
 
     f.close()
 
@@ -47,7 +53,6 @@ def main(argv):
     client.run(datalist[0])
 
 
-
 def CreateBuildMessage(fileName, downloadLink, version):
     global messageText
 
@@ -59,7 +64,23 @@ def CreateBuildMessage(fileName, downloadLink, version):
     messageText += "버전 : " + version + "\n"
 
 
+def EncryptionToken(code):
+    for s in code:
+        code
+
+
+def SaveBinaryToken():
+
+    f = open("/Users/shj/Desktop/UnityProject/Project_JPER/JPER_Bot/PGBot/data.bin",
+             mode='wb')
+
+    tokenlist = ""
+    tokenlist += "discordCode:Njc3NTcwMTU5NTIyODczMzc0.Xky7SQ.mFJ-y04Sb1GLdYBOUF8gygmNH4k\n"
+    tokenlist += "channelID:677569915963572224\n"
+    tokenlist += "dropBoxCode:3O3J4VcCmqAAAAAAAAAAqteidViPilrvA7dc826_k9Ym1lWmdtWvQI9ES22QWJvU\n"
+    tokenlist += "dropboxUplodePath:/JPER/Build/\n"
+
+    f.write(tokenlist.encode())
+
 if __name__ == "__main__":
     main(sys.argv)
-
-
